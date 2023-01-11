234b9646551b867d073e3bc986b416178f9da4ee
Bigardcode <84800557+Bigardcode@users.noreply.github.com>
2023-01-11 13:34:19 +0330

    Update ArleQuinInPuts.sh

--- ArleQuinInPuts.sh
+++ ArleQuinInPuts.sh
@@ -1,21 +1,21 @@
-################How to Make input(.arp/.hap) for Arlequin from VCF file####################### 
+################ How to Make input(.arp/.hap) for Arlequin from VCF file ##################
 
 
-########Setup VCF2ArlequinDiploid---------------------------------------------------------
+######## Setup VCF2ArlequinDiploid--------------------------------------------------------
 
 $ wget https://github.com/Sandman2127/VCF2ArlequinDiploid/archive/refs/heads/master.zip
 $ gunzip master.zip
 
 
-########run vcf2ArlequinDiploid analysis---------------------------------------------------
+######## Run vcf2ArlequinDiploid analysis--------------------------------------------------
 
 
-###split the input .vcf data into several output .arp files by contig
+### Split the input .vcf data into several output .arp files by contig
 
 $ python vcf2ArlequinDiploid.py --vcf snps.vcf --popFile PoPulation.txt  --splitContigs
 
 
-###Merge the input .vcf data into one output .arp file
+### Merge the input .vcf data into one output .arp file
 
 $ python vcf2ArlequinDiploid.py --vcf snps.vcf --popFile PoPulation.txt
 
